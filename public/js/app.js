f961bacdf86a00e94d34d40f0deba362b7326f91
gladysaclan <aclan.gladysa@gmail.com>
2021-08-30 18:19:51 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,7 @@ fetch('http://localhost:3000/weather?address=!').then((response) => {
         }
     })
 })
-*/
+
 //////////////////////////////////////////////
 // Creating a Search Form
 
@@ -78,3 +78,50 @@ weatherForm.addEventListener('submit', (e) => {
 // 1. Migrate fetch call into the submit call
 // 2. Use the search text as the address  query string value
 // 3. Submit the form with a valid and invalid value to test
+*/
+//////////////////////////////////////////////
+// Integrating Git
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
+
+// 
+// Goal: Render content to paragraphs
+// 
+// 1. Select the second message p from Javascript
+// 2. Just before fetch, render loading message and empty p
+// 3. If no error, render location and forecast
+// 5. Test your work! Search for errors and for valid locations
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+
+})
+
+// 
+// Goal: Use input value to get weather
+// 
+// 1. Migrate fetch call into the submit call
+// 2. Use the search text as the address  query string value
+// 3. Submit the form with a valid and invalid value to test
+
